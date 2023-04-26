Fixes off-by-one error and changes drop_last for Cracks
- The off-by-one error in make_dataset in the VideoLoader was causing us
  to drop the last sample of our datasets.
	- Increases samples available for the training set from 90 -> 91
	- Increases samples available for the testing set from 10 -> 11
- Changing drop_last=True to False allows for the full dataset to be
  utilized for testing. Increases test set from 8 -> 11
- Left drop_last=True for the training dataloader for consistency

Changes drop_last to True for training dataloader

simvp/datasets/dataloader_cracks.py
@@ -31,7 +31,7 @@ def load_data(batch_size, val_batch_size, data_root, num_workers=4, pre_seq_leng
                         #                           num_workers=num_workers)
     dataloader_test = torch.utils.data.DataLoader(test_set,
                                                   batch_size=val_batch_size, shuffle=False,
-                                                  pin_memory=True, drop_last=True,
+                                                  pin_memory=True, drop_last=False,
                                                   num_workers=num_workers)
     
     return dataloader_train, dataloader_vali, dataloader_test